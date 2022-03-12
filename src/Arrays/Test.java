305a6a62e3ca4b0aa53b6bcb6db8ed8dcd7b024e
Redbeard29 <ben@benshermanmusic.com>
2022-03-12 09:07:18 -0500

    initial commit

---
+++ src/Arrays/Test.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class Test {
+
+    public static void main(String[] args){
+        int[] intArray = new int[7];
+
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+        for(int x = 0; x < intArray.length; x++){
+            System.out.println(intArray[x]);
+        }
+    }
+
+}
